60010ce73c49184e4a637811b15353ac56d04d55
Kunal Sambyal <kunalsambyal9628@gmail.com>
2026-05-24 13:40:52 +0530

    divided the logic into smaller functions

--- script.js
+++ script.js
@@ -1,27 +1,43 @@
 const imageList = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg"];
-
 const imageTag = document.getElementById("image");
 const imageNavButtons = document.querySelectorAll(".imageNavButtons");
-
-console.log(imageTag.src);
+let currentIndex = 0;
 
 function changeImage(path) {
     imageTag.src = `/${path}`;
 }
 
-let currentIndex = 0;
+function checkForIndex(indexToCheck) {
+    if (indexToCheck >= imageList.length) {
+        return 0;
+    }
+    if (indexToCheck < 0) {
+        return imageList.length - 1;
+    }
+    return indexToCheck;
+}
+
+function nextImage() {
+    currentIndex++;
+    currentIndex = checkForIndex(currentIndex);
+    changeImage(imageList[currentIndex]);
+}
+
+function prevImage() {
+    currentIndex--;
+    currentIndex = checkForIndex(currentIndex);
+    changeImage(imageList[currentIndex]);
+}
 
 imageNavButtons.forEach((button) => {
     button.addEventListener("click", (event) => {
-        if (String(event.target.id).startsWith("right")) currentIndex++;
-        if (String(event.target.id).startsWith("left")) currentIndex--;
-
-        if (currentIndex >= imageList.length) {
-            currentIndex = 0;
+        if (event.target.id === "rightImageButton") {
+            nextImage();
         }
-        if (currentIndex < 0) {
-            currentIndex = imageList.length - 1;
+        if (event.target.id === "leftImageButton") {
+            prevImage();
         }
-        changeImage(imageList[currentIndex]);
     });
 });
+
+// document.addEventListener("DOMContentLoaded", intializeSlider);
